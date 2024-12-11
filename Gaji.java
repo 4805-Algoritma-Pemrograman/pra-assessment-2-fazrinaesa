a0693123d826ef7c5760501511e4fd893ec40690
fazrinaesa <fazrinaesa2005@gmail.com>
2024-12-11 22:49:34 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -1,45 +1,53 @@
 import java.util.Scanner;
 
-public class Gaji1{
-    public static int masaKerja;
+public class HitungGaji {
+    static int masaKerja;
 
-        public static int Gaji() {
-            int gaji = 0;
-            if (masaKerja < 5) {
-                gaji = 5000000;
-            } else if (masaKerja >= 5 && masaKerja <= 10) {
-                gaji = 7500000;
-            } else {
-                gaji = 10000000;
-            }
-            return gaji;
+    public static int hitungGaji() {
+        if (masaKerja < 5) {
+            return 5000000;
+        } else if (masaKerja >= 5 && masaKerja <= 10) {
+            return 7500000;
+        } else {
+            return 10000000;
         }
+    }
 
-        public static int honorLembur(int jamLembur) {
-            int honor = 0;
-            if (masaKerja < 5) {
-                honor = 40000;
-            } else if (masaKerja >= 5 && masaKerja <= 10) {
-                honor = 50000;
-            } else if (masaKerja > 10) {
-                honor = 75000;
-            }
-            return honor * jamLembur;
+    public static int hitungUangLembur(int jumlahJamLembur) {
+        int honorPerJam;
+        if (masaKerja < 5) {
+            honorPerJam = 40000;
+        } else if (masaKerja >= 5 && masaKerja <= 10) {
+            honorPerJam = 50000;
+        } else {
+            honorPerJam = 75000;
         }
-    
-    public static void main(String[] args){
-    
-            Scanner input = new Scanner(System.in);
+        return honorPerJam * jumlahJamLembur;
+        // 50000 * 6 = 300000
+    }
 
-            System.out.print("Masukan masa kerja : ");
-            masaKerja = input.nextInt();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Masukan jam lembur : ");
-            int jamLembur = input.nextInt();
-    
-            /* Pekerjaan anda mulai dari sini */
-            /* Pekerjaan anda berakhir sini */
-    
-            System.out.println(honorLembur(jamLembur) + Gaji());
-        }
+        System.out.print("Masukkan masa kerja (dalam tahun): ");
+        masaKerja = scanner.nextInt();
+        // 5
+
+        System.out.print("Masukkan jumlah jam lembur: ");
+        int jumlahJamLembur = scanner.nextInt();
+        // 6
+
+        int gaji = hitungGaji();
+        // 7500000
+        int uangLembur = hitungUangLembur(jumlahJamLembur);
+        // 300000
+
+        int totalGaji = gaji + uangLembur;
+        // 7800000
+
+        System.out.println("Gaji pokok: " + gaji);
+        System.out.println("Uang lembur: " + uangLembur);
+        System.out.println("Total gaji: " + totalGaji);
+        
+    }
 }
